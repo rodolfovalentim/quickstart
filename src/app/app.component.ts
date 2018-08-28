d86f53a95f59b6f93324bda20e999086bdba9afa
Rodolfo Vieira Valentim <rodolfovieiravalentim@gmail.com>
2018-08-28 20:15:03 -0300

    Revert "Login Page"
    This reverts commit c13e17bd20041697adc0f72ec0e0813a6656077f.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {}
+export class AppComponent {
+  title = 'quickstart';
+  images = ['https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/EBay_logo.svg/2000px-EBay_logo.svg.png',
+            'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Microsoft_logo_%282012%29.svg/2000px-Microsoft_logo_%282012%29.svg.png'];
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule }   from '@angular/forms';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
-
-import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { RegisterPageComponent } from './pages/register-page/register-page.component';
-import { JackpotPageComponent } from './pages/jackpot-page/jackpot-page.component';
-import { CameraPageComponent } from './pages/camera-page/camera-page.component';
-import { PhotoPageComponent } from './pages/photo-page/photo-page.component';
-import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginPageComponent,
-    RegisterPageComponent,
-    JackpotPageComponent,
-    CameraPageComponent,
-    PhotoPageComponent,
-    DashboardPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
-    MatButtonModule,
-    AppRoutingModule,
-    FormsModule,
-    MatFormFieldModule,
-    MatInputModule
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
